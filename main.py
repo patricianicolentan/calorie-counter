4daf76fb19cdeb9715f4ff1636e858d7fb9a950b
Ligert <ligertsicat@gmail.com>
2021-11-17 11:29:12 +0800

    un-hardcode port

--- main.py
+++ main.py
@@ -512,4 +512,5 @@ def diary():
 	return render_template('diary.html')
 
 if __name__ == "__main__":
-	app.run(host="127.0.0.1", port=8080, debug=True)
+	port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
