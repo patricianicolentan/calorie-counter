50067e2855d1fc75e369597c75b7d139cc73a31c
Patricia <patricianicolentan@gmail.com>
2021-11-20 19:53:48 +0800

    work on diary issue

--- main.py
+++ main.py
@@ -469,224 +469,221 @@ def adddiarylog():
 
 
 	if request.method == "POST":
+		datex = request.form['date']
+
+		if not datex:
+			return "You must input a date."
+
+		
+		weightx = request.form['weight']
+		exercisedx = request.form['exercised']
+		overatex = request.form['overate']
+		breakfastx = request.form['breakfast']
+		lunchx = request.form['lunch']
+		dinnerx = request.form['dinner']
+		snack1x = request.form['snack1']
+		snack2x = request.form['snack2']
+		snack3x = request.form['snack3']
+		snack4x = request.form['snack4']
+		snack5x = request.form['snack5']
+		snack6x = request.form['snack6']
+		bcalories = request.form['bcalories']
+		lcalories = request.form['lcalories']
+		dcalories = request.form['dcalories']
+		s1calories = request.form['s1calories']
+		s2calories = request.form['s2calories']
+		s3calories = request.form['s3calories']
+		s4calories = request.form['s4calories']
+		s5calories = request.form['s5calories']
+		s6calories = request.form['s6calories']
+
+
+
+		try: 
+			conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+		except:
+			return "1"
+
 		try:
-			datex = request.form['date']
-
-			if not datex:
-				return "You must input a date."
-
-
-			weightx = request.form['weight']
-			exercisedx = request.form['exercised']
-			overatex = request.form['overate']
-			breakfastx = request.form['breakfast']
-			lunchx = request.form['lunch']
-			dinnerx = request.form['dinner']
-			snack1x = request.form['snack1']
-			snack2x = request.form['snack2']
-			snack3x = request.form['snack3']
-			snack4x = request.form['snack4']
-			snack5x = request.form['snack5']
-			snack6x = request.form['snack6']
-			bcalories = request.form['bcalories']
-			lcalories = request.form['lcalories']
-			dcalories = request.form['dcalories']
-			s1calories = request.form['s1calories']
-			s2calories = request.form['s2calories']
-			s3calories = request.form['s3calories']
-			s4calories = request.form['s4calories']
-			s5calories = request.form['s5calories']
-			s6calories = request.form['s6calories']
+			cur = conn.cursor()
+		except:
+			return "2"
+		
 
 
 
-			try: 
-				conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-			except:
-				return "1"
+	#update diary set weight = null where date = ('2021-08-08');
+		try:	
+			# 		cur.execute("UPDATE calorielog set servingsize, servinggrams, caloriesperserving = '{}, {}, {}' where foodname = '{}'".format(
+		#servingsize, servinggrams, caloriesperserving, foodname))
+			cur.execute("INSERT INTO diary (date) VALUES ('{}')".format(datex))
+			print("date written")
+		except:
+			print("diary fail 1")
+
+		if weightx is not NULL:
 
 			try:
-				cur = conn.cursor()
+				cur.execute("UPDATE diary SET weight = '{}' where date ='{}'".format(weightx, datex))
+				print("w")
 			except:
-				return "2"
-			
+				print("diary fail 2")
 
-
-
-		#update diary set weight = null where date = ('2021-08-08');
-			try:	
-				# 		cur.execute("UPDATE calorielog set servingsize, servinggrams, caloriesperserving = '{}, {}, {}' where foodname = '{}'".format(
-			#servingsize, servinggrams, caloriesperserving, foodname))
-				cur.execute("INSERT INTO diary (date) VALUES ('{}')".format(datex))
-				print("date written")
-			except:
-				print("diary fail 1")
-
-			if weightx is not NULL:
-
-				try:
-					cur.execute("UPDATE diary SET weight = '{}' where date ='{}'".format(weightx, datex))
-					print("w")
-				except:
-					print("diary fail 2")
-
-			if exercisedx is not NULL:
-				try:
-					cur.execute("UPDATE diary SET exercised = '{}' where date ='{}'".format(exercisedx, datex))
-					print("e")
-
-				except:
-					print("diary fail 3")
-
-			if overatex is not NULL:
-				try:
-					cur.execute("UPDATE diary SET overate = {} where date ='{}'".format(overatex, datex))
-					print("o")
-
-				except:
-					print("diary fail 4")
-
-			if breakfastx is not NULL:
-				try:
-					cur.execute("UPDATE diary SET breakfast = '{}' where date ='{}'".format(breakfastx, datex))
-
-				except:
-					print("diary fail 5")
-
-			if lunchx is not NULL:
-				try:
-					cur.execute("UPDATE diary SET lunch = '{}' where date ='{}'".format(lunchx, datex))
-				except:
-					print("diary fail 6")
-
-			if dinnerx is not NULL:
-				try:
-					cur.execute("UPDATE diary SET dinner = '{}' where date ='{}'".format(dinnerx, datex))
-				except:
-					print("diary fail 7")
-
-			if snack1x is not NULL:		
-				try:
-					cur.execute("UPDATE diary SET snack1 = '{}' where date ='{}'".format(snack1x, datex))
-				except:
-					print("diary fail 8")
-			if snack2x is not NULL:
-				try:
-					cur.execute("UPDATE diary SET snack2 = '{}' where date ='{}'".format(snack2x, datex))
-				except:
-					print("diary fail 9")
-
-			if snack3x is not NULL:
-				try:
-					cur.execute("UPDATE diary SET snack3 = '{}' where date ='{}'".format(snack3x, datex))
-				except:
-					print("diary fail 10")
-
-			if snack4x is not NULL:
-				try:
-					cur.execute("UPDATE diary SET snack4 = '{}' where date ='{}'".format(snack4x, datex))
-				except:
-					print("diary fail 11")
-
-			if snack5x is not NULL:
-				try:
-					cur.execute("UPDATE diary SET snack5 = '{}' where date ='{}'".format(snack5x, datex))
-				except:
-					print("diary fail 12")
-
-			if snack6x is not NULL:
-				try:
-					cur.execute("UPDATE diary SET snack6 = '{}' where date ='{}'".format(snack6x, datex))
-				except:
-					print("diary fail 13")
-
-			if bcalories is not NULL:
-				try:
-					cur.execute("UPDATE diary SET bcalories = {} where date = '{}'".format(bcalories, datex))
-				except:
-					print("diary fail 14")
-
-			if lcalories is not NULL:
-				try:
-					cur.execute("UPDATE diary SET lcalories = {} where date = '{}'".format(lcalories, datex))
-				except:
-					print("diary fail 15")
-
-			if dcalories is not NULL:
-				try:
-					cur.execute("UPDATE diary SET dcalories = {} where date = '{}'".format(dcalories, datex))
-				except:
-					print("diary fail 16")
-
-			if s1calories is not NULL:
-				try:
-					cur.execute("UPDATE diary SET s1calories = {} where date = '{}'".format(s1calories, datex))
-				except:
-					print("diary fail 17")
-
-			if s2calories is not NULL:
-				try:
-					cur.execute("UPDATE diary SET s2calories = {} where date = '{}'".format(s2calories, datex))
-				except:
-					print("diary fail 18")
-
-			if s3calories is not NULL:
-				try:
-					cur.execute("UPDATE diary SET s3calories = {} where date = '{}'".format(s3calories, datex))
-				except:
-					print("diary fail 19")
-
-			if s4calories is not NULL:
-				try:
-					cur.execute("UPDATE diary SET s4calories = {} where date = '{}'".format(s4calories, datex))
-				except:
-					print("diary fail 20")
-
-			if s5calories is not NULL:
-				try:
-					cur.execute("UPDATE diary SET s5calories = {} where date = '{}'".format(s5calories, datex))
-				except:
-					print("diary fail 21")
-
-			if s6calories is not NULL:
-				try:
-					cur.execute("UPDATE diary SET s6calories = {} where date = '{}'".format(s6calories, datex))
-				except:
-					print("diary fail 22")
-
-
+		if exercisedx is not NULL:
 			try:
-				conn.commit()
-				print("committed")
+				cur.execute("UPDATE diary SET exercised = '{}' where date ='{}'".format(exercisedx, datex))
+				print("e")
+
 			except:
-				print("commit diary log failed")
+				print("diary fail 3")
 
-
-				#("INSERT INTO diary (weight, exercised, overate, breakfast, lunch, dinner, snack1, snack2, snack3, snack4, snack5, snack6) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (weightx, exercisedx, overatex, breakfastx, lunchx, dinnerx, snack1x, snack2x, snack3x, snack4x, snack5x, snack6x))
-			#except:
-				#print("fail")
-				#else:
-					#print("else")
-					#pass
-
-			#print("ok4")
-
-			print("Finis!")
+		if overatex is not NULL:
 			try:
-				cur.execute("SELECT date, weight, exercised, overate, breakfast, bcalories, lunch, lcalories, dinner, dcalories, snack1, s1calories, snack2, s2calories, snack3, s3calories, snack4, s4calories, snack5, s5calories, snack6, s6calories, maxcalories, totalcalories FROM diary")
-				#cur.execute("SELECT * FROM diary")
+				cur.execute("UPDATE diary SET overate = {} where date ='{}'".format(overatex, datex))
+				print("o")
+
 			except:
-				print("diary get failed")
-			#cur.execute("SELECT date, max(weight), max(exercised), max(overate), max(breakfast), max(bcalories), max(lunch), max(lcalories), max(dinner), max(dcalories), max(snack1), max(s1calories), max(snack2), max(s2calories), max(snack3), max(s3calories), max(snack4), max(s4calories), max(snack5), max(s5calories), max(snack6), max(s6calories) FROM diary group by date ORDER BY date desc")
-			diary = cur.fetchall()
-			conn.close()
-			return render_template('diary.html', diary = diary)
+				print("diary fail 4")
+
+		if breakfastx is not NULL:
+			try:
+				cur.execute("UPDATE diary SET breakfast = '{}' where date ='{}'".format(breakfastx, datex))
+
+			except:
+				print("diary fail 5")
+
+		if lunchx is not NULL:
+			try:
+				cur.execute("UPDATE diary SET lunch = '{}' where date ='{}'".format(lunchx, datex))
+			except:
+				print("diary fail 6")
+
+		if dinnerx is not NULL:
+			try:
+				cur.execute("UPDATE diary SET dinner = '{}' where date ='{}'".format(dinnerx, datex))
+			except:
+				print("diary fail 7")
+
+		if snack1x is not NULL:		
+			try:
+				cur.execute("UPDATE diary SET snack1 = '{}' where date ='{}'".format(snack1x, datex))
+			except:
+				print("diary fail 8")
+		if snack2x is not NULL:
+			try:
+				cur.execute("UPDATE diary SET snack2 = '{}' where date ='{}'".format(snack2x, datex))
+			except:
+				print("diary fail 9")
+
+		if snack3x is not NULL:
+			try:
+				cur.execute("UPDATE diary SET snack3 = '{}' where date ='{}'".format(snack3x, datex))
+			except:
+				print("diary fail 10")
+
+		if snack4x is not NULL:
+			try:
+				cur.execute("UPDATE diary SET snack4 = '{}' where date ='{}'".format(snack4x, datex))
+			except:
+				print("diary fail 11")
+
+		if snack5x is not NULL:
+			try:
+				cur.execute("UPDATE diary SET snack5 = '{}' where date ='{}'".format(snack5x, datex))
+			except:
+				print("diary fail 12")
+
+		if snack6x is not NULL:
+			try:
+				cur.execute("UPDATE diary SET snack6 = '{}' where date ='{}'".format(snack6x, datex))
+			except:
+				print("diary fail 13")
+
+		if bcalories is not NULL:
+			try:
+				cur.execute("UPDATE diary SET bcalories = {} where date = '{}'".format(bcalories, datex))
+			except:
+				print("diary fail 14")
+
+		if lcalories is not NULL:
+			try:
+				cur.execute("UPDATE diary SET lcalories = {} where date = '{}'".format(lcalories, datex))
+			except:
+				print("diary fail 15")
+
+		if dcalories is not NULL:
+			try:
+				cur.execute("UPDATE diary SET dcalories = {} where date = '{}'".format(dcalories, datex))
+			except:
+				print("diary fail 16")
+
+		if s1calories is not NULL:
+			try:
+				cur.execute("UPDATE diary SET s1calories = {} where date = '{}'".format(s1calories, datex))
+			except:
+				print("diary fail 17")
+
+		if s2calories is not NULL:
+			try:
+				cur.execute("UPDATE diary SET s2calories = {} where date = '{}'".format(s2calories, datex))
+			except:
+				print("diary fail 18")
+
+		if s3calories is not NULL:
+			try:
+				cur.execute("UPDATE diary SET s3calories = {} where date = '{}'".format(s3calories, datex))
+			except:
+				print("diary fail 19")
+
+		if s4calories is not NULL:
+			try:
+				cur.execute("UPDATE diary SET s4calories = {} where date = '{}'".format(s4calories, datex))
+			except:
+				print("diary fail 20")
+
+		if s5calories is not NULL:
+			try:
+				cur.execute("UPDATE diary SET s5calories = {} where date = '{}'".format(s5calories, datex))
+			except:
+				print("diary fail 21")
+
+		if s6calories is not NULL:
+			try:
+				cur.execute("UPDATE diary SET s6calories = {} where date = '{}'".format(s6calories, datex))
+			except:
+				print("diary fail 22")
+
+
+		try:
+			conn.commit()
+			print("committed")
+		except:
+			print("commit diary log failed")
+
+
+			#("INSERT INTO diary (weight, exercised, overate, breakfast, lunch, dinner, snack1, snack2, snack3, snack4, snack5, snack6) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (weightx, exercisedx, overatex, breakfastx, lunchx, dinnerx, snack1x, snack2x, snack3x, snack4x, snack5x, snack6x))
+		#except:
+			#print("fail")
+			#else:
+				#print("else")
+				#pass
+
+		#print("ok4")
+
+		print("Finis!")
+		try:
+			cur.execute("SELECT date, weight, exercised, overate, breakfast, bcalories, lunch, lcalories, dinner, dcalories, snack1, s1calories, snack2, s2calories, snack3, s3calories, snack4, s4calories, snack5, s5calories, snack6, s6calories, maxcalories, totalcalories FROM diary")
+			#cur.execute("SELECT * FROM diary")
+		except:
+			print("diary get failed")
+		#cur.execute("SELECT date, max(weight), max(exercised), max(overate), max(breakfast), max(bcalories), max(lunch), max(lcalories), max(dinner), max(dcalories), max(snack1), max(s1calories), max(snack2), max(s2calories), max(snack3), max(s3calories), max(snack4), max(s4calories), max(snack5), max(s5calories), max(snack6), max(s6calories) FROM diary group by date ORDER BY date desc")
+		diary = cur.fetchall()
+		conn.close()
+		return render_template('diary.html', diary = diary)
 
 
 		# end
-		except:
-			return "Database Connection Error"
-			return render_template('adddiarylog.html')
+		
 
 	else:
 		conn = psycopg2.connect(DATABASE_URL, sslmode='require')
